e0f364aa9f6ad33b99a18a76b0442466281b2f5e
Dmitry Buevich <buevichd@yandex-team.ru>
2021-04-06 23:36:48 +0300

    Just commit

--- build.sh
+++ build.sh
@@ -6,13 +6,14 @@ if [[ -z "$GITHUB_RUN_NUMBER" ]]; then
     export GITHUB_RUN_NUMBER=1
 fi
 
-ROOT_DIR=$(pwd)
-BIN_DIR=$ROOT_DIR/bin
+export ROOT_DIR=$(pwd)
+export BIN_DIR=$ROOT_DIR/bin
 
 mkdir -p $BIN_DIR
 cd $BIN_DIR
 cmake $ROOT_DIR
 cmake --build .
 ctest .
+cpack .
 
 echo "Build was completed successfully!"
