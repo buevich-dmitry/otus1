ccf5d168c50edaf61d4d13138fbd4c9807c339e2
Dmitry Buevich <buevichd@yandex-team.ru>
2021-04-06 22:59:01 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
+set -e
+
 # 1 means local build
 export GITHUB_RUN_NUMBER=1
 
 ROOT_DIR=$(pwd)
 BIN_DIR=$ROOT_DIR/bin
 
+mkdir -p $BIN_DIR
 cd $BIN_DIR
 cmake $ROOT_DIR
 cmake --build .
 ctest .
+
+echo "Build was completed successfully!"
